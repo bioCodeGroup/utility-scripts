ab118071031fa950d11794117db1603e3c41a0d5
Charlotte Cialek <ccialek@gmail.com>
2018-09-13 17:33:51 -0600

    Deleted old code moved it into qseq to fastq

--- qseq2fastq.py
+++ qseq2fastq.py
@@ -4,12 +4,12 @@ from __future__ import absolute_import
 import argparse
 
 def get_args():
-    """ 
+    """
     Get input arguments
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--input-file', nargs="+", 
+    parser.add_argument('-f', '--input-file', nargs="+",
                         metavar='QSEQFILES', help='qseq files')
     parser.add_argument('-o', '--output-file', nargs="+",
                         metavar='FASTQFILES', help='fastq filenames')
@@ -20,16 +20,27 @@ def get_args():
 
 ## To do:
 
-## add function to read entries from qseq 
-##
+f = open("sampleqseq2.txt","r")
+lib = {}
+sep = ":"
+for line in f:
+    a = line.split('|')
+    headers = "@" + sep.join(a[0:8])
+    seq = a[8]
+    qual = a[9]
+    filtering = a[21].strip()
+    #print(headers)
+    #print (seq)
+    #print(qual)
+    #print (filtering)
 ## add function to filter out bad entries
 ##
 
 
 def main():
-    """ 
+    """
     Main routine
-    
+
     Convert qseq file to fastq file
     """
 
