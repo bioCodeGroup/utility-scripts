a723cb89fe072368132962274edf196bc5cd31fb
Charlotte Cialek <ccialek@gmail.com>
2018-09-25 16:20:39 -0600

    metadata file create
    added code to create a metadata file as an aoutput

--- qseq2fastq.py
+++ qseq2fastq.py
@@ -15,18 +15,29 @@ def get_args():
                         metavar='QSEQFILES', help='qseq files')
     parser.add_argument('-o', '--output-file',
                         metavar='FASTQFILES', help='fastq filenames')
+    parser.add_argument('-m', '--metadata-file',
+                        metavar='METADATAFILES', help='metadata filenames')
+
 
     args = parser.parse_args()
 
     return args
 
-def qseq_to_fastq(qseq_file, fastq_file):
+def qseq_to_fastq(qseq_file, fastq_file, metadata_file):
     """
     convert qseq file to fastq file
     """
     # list into which the qseq file will be read,
     # and from which the fastq file will be built
+    # Also, pass_count and fail_count count the
+    # input file's lines which either pass/fail
+    # quality control and testing to make sure
+    # the line is actually part of the qseq file
+
     qseq_list = []
+    pass_count = 0
+    fail_count = 0
+
     try:
         with open(qseq_file) as f:
             #loop that populates qseq_list
@@ -36,12 +47,18 @@ def qseq_to_fastq(qseq_file, fastq_file):
                 # format that didn't meet quality standards
                 if len(fields) == 11 and fields[10] == '1':
                     qseq_list = qseq_list + [fields]
+                    pass_count += 1
+                else:
+                    fail_count +=1
     except:
         return 'Unable to open input file'
 
     with open(fastq_file, 'w') as output:
         for item in qseq_list:
             output.writelines('@'+":".join(item[:8])+'\n'+item[8]+'\n+\n'+item[9]+'\n')
+
+    with open(metadata_file, 'w') as mdat:
+            mdat.writelines('Input qseq file: ' + str(qseq_file) + '\n' + 'Output fastq file: ' + str(fastq_file) + '\n' + 'Number of lines passing Quality Control: ' + str(pass_count) + '\n' + 'Number of lines failing Quality Control or not proper qseq file lines: ' + str(fail_count))
 
 def main():
     """
@@ -51,7 +68,7 @@ def main():
     """
 
     args = get_args()
-    qseq_to_fastq(args.input_file, args.output_file)
+    qseq_to_fastq(args.input_file, args.output_file, args.metadata_file)
 
 if __name__ == '__main__':
     main()
